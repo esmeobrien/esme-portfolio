55ec0ee5524107c2a365f21b03af8a311aa5dde4
Esmé O'Brien <eobrien874@gmail.com>
2025-06-13 21:42:17 +0100

    Helmet and Meta name added to home page

--- src/App.js
+++ src/App.js
@@ -1,13 +1,18 @@
 import './App.css';
 import About from "./Pages/About/About.jsx";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
-
+import { Helmet } from 'react-helmet';  
 import Header from "./Components/Header/Header.jsx";
 import Footer from './Components/Footer/Footer.jsx';
 import Home from './Pages/Home/Home.jsx';
 
 function App() {
   return (
+    <> 
+    <Helmet>
+      <title>Esmé O'Brien - Manchester Based Junior Developer</title>
+      <meta name="description" content="Esmé O'Brien is a Manchester-based Junior Developer with a passion for front-end development, UI design, and customer engagement. Explore my portfolio to see my work and connect with me." />
+    </Helmet>
     <Router>
       <div className="App">
         <Header/>
@@ -20,6 +25,7 @@ function App() {
         <Footer />
       </div>
     </Router>
+    </>
   );
 }
 
